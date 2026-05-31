b246fa48791bccda5f4caf899660a4ce172b260b
Poppy Amriyati <88833563+poppyamriyati@users.noreply.github.com>
2026-05-31 20:04:39 +0700

    Update load.py

--- etl/load.py
+++ etl/load.py
@@ -1,1 +1,13 @@
-# load to PostgreSQL
+import pandas as pd
+
+customer = pd.read_csv("../data/clean_customer.csv")
+product = pd.read_csv("../data/clean_product.csv")
+sales = pd.read_csv("../data/clean_sales.csv")
+
+print("Loading clean datasets...")
+
+print(f"Customer rows: {len(customer)}")
+print(f"Product rows: {len(product)}")
+print(f"Sales rows: {len(sales)}")
+
+print("Load completed.")
