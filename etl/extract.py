e19ed7bc0a791fd498d5d40fcad139ed6ba1dd18
Poppy Amriyati <88833563+poppyamriyati@users.noreply.github.com>
2026-05-31 19:57:31 +0700

    Update extract.py

--- etl/extract.py
+++ etl/extract.py
@@ -3,3 +3,7 @@ import pandas as pd
 customer = pd.read_csv("../data/customer.csv")
 product = pd.read_csv("../data/product.csv")
 sales = pd.read_csv("../data/sales.csv")
+
+print("Customer rows:", len(customer))
+print("Product rows:", len(product))
+print("Sales rows:", len(sales))
